698614b5f52fe18b5e02ac034ebe7e2f531bde20
Luis Gabriel Oliveira <97142989+luisgabriel1@users.noreply.github.com>
2022-12-19 23:31:10 -0300

    Update Console_E_entrada.java

--- Console_E_entrada.java
+++ Console_E_entrada.java
@@ -2,35 +2,35 @@ package Udemy;
 
 import java.util.Scanner;
 
-public class Console_E_entrada {
+public class Console_the_in {
     public static void main(String[] args) {
-        System.out.println("Bom");
-        System.out.println("dia!\n\n");
+        System.out.println("Good");
+        System.out.println("Morning!\n\n");
 
         System.out.println();
         System.out.println();
 
         System.out.printf("Megasena: %d %d %d %d %d %d %n",
                 1, 2, 3, 4, 5, 6);
-        System.out.printf("Salario: %.1f %n",
+        System.out.printf("Salary: %.1f %n",
                 1234.5678);
-        System.out.printf("Nome: %s %n",
+        System.out.printf("name: %s %n",
                 "Luis");
 
 
 
-        Scanner entrada = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
 
-        System.out.print("Digite seu nome: ");
-        String nome = entrada.nextLine();
+        System.out.print("Inform your first name: ");
+        String FirstName = in.nextLine();
 
-        System.out.print("Digite seu sobrenome: ");
-        String sobrenome = entrada.nextLine();
+        System.out.print("Inform your last name: ");
+        String LastName = in.nextLine();
 
-        System.out.print("Digite sua idade: ");
-        int idade = entrada.nextInt();
+        System.out.print("inform your age: ");
+        int Age = in.nextInt();
 
-        System.out.printf("%s %s tem %d anos.%n",
-                nome, sobrenome, idade);
+        System.out.printf("%s %s have %d age.%n",
+                FirstName, LastName, Age);
     }
 }
